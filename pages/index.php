0fb1acf4bd813ff0d89a2882a54751af3e507b17
acs <a.bederiat@codeur.online>
2021-06-29 16:57:32 +0200

    portfolio

---
+++ pages/index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Adam Bederiat</title>
+
+    <link rel="stylesheet" href="../styles/main.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css">
+
+
+</head>
+<body>
+    <header>
+        <nav>
+            <ul class = "navbar">
+                <li id="logo"> <a href="#">A.B</a> </li>
+                <li> <a href="#langue">Projet</a> </li>
+                <li> <a href="#reserver">CV</a> </li>
+                <li> <a href="#contact">Me contacter</a> </li>
+            
+            </ul>
+        </nav>
+        <div id="imagePrincipale">
+            <h1>Adam Bederiat</h1>
+            <div id="premierTrait"></div>
+            <h3>Décourvez moi</h3>
+        </div>
+    </header>
+
+
+        <section id="presentation">
+            <div id="texteIntro">
+                <h2>Un lieu unique pour un séjour unique</h2>
+                <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi dolorem ratione eius odio velit qui, quo inventore incidunt iusto facilis illo nostrum blanditiis! Quis quod nihil consequuntur tempora odit aspernatur dolorum deleniti, maiores amet perferendis eveniet possimus eius recusandae sed necessitatibus, corporis nam consequatur. Vero eligendi esse earum id tempora soluta laborum, minima minus est? Saepe officia placeat soluta eius dolor, repudiandae alias accusamus quod aperiam eum suscipit velit ex vero magni fuga iure debitis? Vitae dolore tempora qui a.</p>
+            </div>
+            <div class ="portfolio">
+                <div class ="image active" style="background: url(../assets/images/capture.PNG) center/cover;"></div>
+                <div class ="image" style="background: url(../assets/images/capture2.PNG) center/cover;"></div>
+                <div class ="image" style="background: url(../assets/images/capture3.PNG) center/cover;"></div>
+                <div class ="image" style="background: url(../assets/images/capture4.PNG) center/cover;"></div>
+                <div class ="image" style="background: url(../assets/images/capture5.jpg) center/cover;"></div>
+            </div>
+        </section>
+
+    <footer>
+        <h2>Contactez-nous</h2>
+        <form>
+            <input placeholder="Nom">
+            <input placeholder="E-mail">
+            <textarea placeholder="Votres message ici..."></textarea>
+            <button>Envoyer</button>
+        </form>
+        <div id="deuxiemeTrait"></div>
+        <div id="copyrightEtIcons"></div>
+        <div id="copyright">
+            <span>© from le plus beau des rebeu</span>
+        </div>
+        <div id="icons">
+            <a href="http://www.twitter.fr"><i class="fa fa-twitter"></i> </a>
+            <a href="http://www.facebook.fr"><i class="fa fa-facebook"></i></a>            
+            <a href="http://www.instagram.com"><i class="fa fa-instagram"></i></a>            
+
+        </div>
+    </footer>
+    
+    <script
+  src="https://code.jquery.com/jquery-3.6.0.min.js"
+  integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
+  crossorigin="anonymous"></script>
+  <script src="../script/script.js"></script>
+
+</body>
+</html>
